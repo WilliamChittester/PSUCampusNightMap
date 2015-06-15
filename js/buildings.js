2f4a7e248f0aea9f587faf05ff016b2257f4bb7f
William Chittester <wcc5043@psu.edu>
2015-06-15 14:56:59 -0400

    Update buildings.js

--- js/buildings.js
+++ js/buildings.js
@@ -15,16 +15,7 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
   // Adding all the building centroids as a points layer
   var markers = L.mapbox.featureLayer(centroids) // Creates a new feature layer from the GeoJSON data `centroids`
     .setFilter(function() { return false; }) // Filters out all of the data so no points actually appear on the map. We'll add them when we search for specific points later on.
-  .bindPopup(
-      '<h1>"label"</h1>' +
-      '<ul>' +
-      '<li>Department of This</li>' +
-      '<li>Department of That</li>' +
-      '</ul>' +
-      '<div><img style="margin:2px;width:100%;" src=http://www.facilities.psu.edu/FISWebSite//psufacphotos/0504000.jpg /></div>' +
-      '<button class="btn btn-info" onClick="getDirections()">Directions to here</button>'
-    ) // This "bindPopUp" method adds the above HTML content to the pop-up window. We need to make that content specific to the feature's data.
-    .addTo(map); // Add the new feature layer to the map.
+  
     
   $.widget( "custom.catcomplete", $.ui.autocomplete, {
     _create: function() {
@@ -89,6 +80,17 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
       }
     }
     
+    .bindPopup(
+      'targetID' +
+      '<ul>' +
+      '<li>Department of This</li>' +
+      '<li>Department of That</li>' +
+      '</ul>' +
+      '<div><img style="margin:2px;width:100%;" src=http://www.facilities.psu.edu/FISWebSite//psufacphotos/0504000.jpg /></div>' +
+      '<button class="btn btn-info" onClick="getDirections()">Directions to here</button>'
+    ) // This "bindPopUp" method adds the above HTML content to the pop-up window. We need to make that content specific to the feature's data.
+    
+    .addTo(map); // Add the new feature layer to the map.
     targetLat = centroids.features[targetPointIndex].geometry.coordinates[1];
     targetLon = centroids.features[targetPointIndex].geometry.coordinates[0];
     
