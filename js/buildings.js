5d9eee64cde9cc17335b02298c00eee85d1293ac
William Chittester <wcc5043@psu.edu>
2015-06-15 15:09:18 -0400

    Update buildings.js

--- js/buildings.js
+++ js/buildings.js
@@ -11,55 +11,12 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
   // Because everything we do after this depends on the JSON file being loaded, the above line waits for the JSON file to be loaded,
   // then the browser will proceed with the below code. The data from the JSON file is the variable `centroids`.
 
-  // Defining a function that automatically zooms the map to the feature with the same title as whatever's in the search field.
-  function focusMap() {
-    
-    var targetName = document.getElementById('search').value; // Gets whatever text the user has entered into the search field.
-    
-    
-    // For each of the centroids points, check if the title matches our target, and if it does,
-    // break out of the loop and set the map view to that point,
-    // then filter our markers feature layer so that only the target point is showing.
-    
-    for (var i = 0; i < data.length; i++) { // Initialize the for loop
-      if (data[i].label === targetName) { // For each point, check if the title of the point matches the target
-        var targetID = data[i]["PICTURE ID"]; // Remembers whichever building id it was that matches for use later.
-        break; // Skip the rest of the loop, we already found what we wanted.
-      } else {
-        console.log('not found'); // If we don't find it, and this should never happen, write in the console that we didn't find it.
-      }
-    }
-    
-    for (var i = 0; i < centroids.features.length; i++) { // Initialize the for loop
-      if (centroids.features[i].properties.building_id == targetID) { // For each point, check if the title of the point matches the target
-        var targetPointIndex = i; // Remembers whichever building id it was that matches for use later.
-        break; // Skip the rest of the loop, we already found what we wanted.
-      } else {
-        console.log('not found'); // If we don't find it, and this should never happen, write in the console that we didn't find it.
-      }
-    }
-    
-    targetLat = centroids.features[targetPointIndex].geometry.coordinates[1];
-    targetLon = centroids.features[targetPointIndex].geometry.coordinates[0];
-    
-    // Change the map view to the coordinates of the target point.
-    map.setView(
-      [centroids.features[targetPointIndex].geometry.coordinates[1],  // This will be the latitude of the point
-       centroids.features[targetPointIndex].geometry.coordinates[0]], // This will be the longitude of the point
-       18);
-    
-    // Filter all the features in the markers feature layer so that only the feature with the same title as our target is showing
-    markers.setFilter(function(feature) { 
-      return feature.properties.building_id == targetID; // Filter the feature with a title property that exactly matches our target.
-    });
-    
-  }
   
   // Adding all the building centroids as a points layer
   var markers = L.mapbox.featureLayer(centroids) // Creates a new feature layer from the GeoJSON data `centroids`
     .setFilter(function() { return false; }) // Filters out all of the data so no points actually appear on the map. We'll add them when we search for specific points later on.
   .bindPopup(
-      'targetPointIndex' +
+      '<h1>Penn State Building</h1>' +
       '<ul>' +
       '<li>Department of This</li>' +
       '<li>Department of That</li>' +
@@ -104,7 +61,49 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
     });
   });
   
-  
+  // Defining a function that automatically zooms the map to the feature with the same title as whatever's in the search field.
+  function focusMap() {
+    
+    var targetName = document.getElementById('search').value; // Gets whatever text the user has entered into the search field.
+    
+    
+    // For each of the centroids points, check if the title matches our target, and if it does,
+    // break out of the loop and set the map view to that point,
+    // then filter our markers feature layer so that only the target point is showing.
+    
+    for (var i = 0; i < data.length; i++) { // Initialize the for loop
+      if (data[i].label === targetName) { // For each point, check if the title of the point matches the target
+        var targetID = data[i]["PICTURE ID"]; // Remembers whichever building id it was that matches for use later.
+        break; // Skip the rest of the loop, we already found what we wanted.
+      } else {
+        console.log('not found'); // If we don't find it, and this should never happen, write in the console that we didn't find it.
+      }
+    }
+    
+    for (var i = 0; i < centroids.features.length; i++) { // Initialize the for loop
+      if (centroids.features[i].properties.building_id == targetID) { // For each point, check if the title of the point matches the target
+        var targetPointIndex = i; // Remembers whichever building id it was that matches for use later.
+        break; // Skip the rest of the loop, we already found what we wanted.
+      } else {
+        console.log('not found'); // If we don't find it, and this should never happen, write in the console that we didn't find it.
+      }
+    }
+    
+    targetLat = centroids.features[targetPointIndex].geometry.coordinates[1];
+    targetLon = centroids.features[targetPointIndex].geometry.coordinates[0];
+    
+    // Change the map view to the coordinates of the target point.
+    map.setView(
+      [centroids.features[targetPointIndex].geometry.coordinates[1],  // This will be the latitude of the point
+       centroids.features[targetPointIndex].geometry.coordinates[0]], // This will be the longitude of the point
+       18);
+    
+    // Filter all the features in the markers feature layer so that only the feature with the same title as our target is showing
+    markers.setFilter(function(feature) { 
+      return feature.properties.building_id == targetID; // Filter the feature with a title property that exactly matches our target.
+    });
+    
+  }
 });
 });
 
