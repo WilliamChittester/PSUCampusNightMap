8110df79e47b2af8fe5d9958df17f75386f3e7ae
William Chittester <wcc5043@psu.edu>
2015-06-16 15:38:21 -0400

    Update buildings.js

--- js/buildings.js
+++ js/buildings.js
@@ -16,12 +16,12 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
   var markers = L.mapbox.featureLayer(centroids) // Creates a new feature layer from the GeoJSON data `centroids`
     .setFilter(function() { return false; }) // Filters out all of the data so no points actually appear on the map. We'll add them when we search for specific points later on.
   .bindPopup(
-      '<h1>"getImage()"</h1>' +
+      'getImage()' +
       '<ul>' +
       '<li>Department of This</li>' +
       '<li>Department of That</li>' +
       '</ul>' +
-      '<div><img style="margin:2px;width:100%;" src= getImage() /></div>' +
+      '<div><img style="margin:2px;width:100%;" src= "getImage()" /></div>' +
       '<button class="btn btn-info" onClick="getDirections()">Directions to here</button>'
     ) // This "bindPopUp" method adds the above HTML content to the pop-up window. We need to make that content specific to the feature's data.
     .addTo(map); // Add the new feature layer to the map.
