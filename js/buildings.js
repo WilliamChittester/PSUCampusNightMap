84967877a37634cddf0c8d27e1deebd8f84832d3
William Chittester <wcc5043@psu.edu>
2015-06-18 15:35:53 -0400

    Update buildings.js

--- js/buildings.js
+++ js/buildings.js
@@ -18,14 +18,14 @@ $.getJSON("https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/building
   var markers = L.mapbox.featureLayer(centroids) // Creates a new feature layer from the GeoJSON data `centroids`
     .setFilter(function() { return false; }) // Filters out all of the data so no points actually appear on the map. We'll add them when we search for specific points later on.
     .bindPopup(
-      <h1>Penn State Building</h1> 
-      <ul>
-      <li>Department of This</li>
-      <li>Department of That</li>
-      </ul>
-      <div>
-      <img style="margin:2px;width:100%;" src="http://www.facilities.psu.edu/FISWebSite//psufacphotos/0023001.jpg" id="building" onclick="changeImage(this)" />
-      </div> 
+      '<h1>Penn State Building</h1>' +
+      '<ul>' +
+      '<li>Department of This</li>' +
+      '<li>Department of That</li>' +
+      '</ul>' +
+      '<div>' +
+      '<img style="margin:2px;width:100%;" src="http://www.facilities.psu.edu/FISWebSite//psufacphotos/0023001.jpg" id="building" onclick="changeImage(this)" />' +
+      '</div>' 
     .addTo(map); // Add the new feature layer to the map.
     
   // Change Image Function
